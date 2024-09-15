13189c6bc5b63a48f305ef37dd49d6274799148d
Irina-eleftheria <ikozlova_work@inbox.ru>
2024-09-15 20:13:16 +0300

    add homework

--- first-react-project/src/App.js
+++ first-react-project/src/App.js
@@ -1,32 +1,33 @@
 import "./App.css";
-import { Box, TextField, Button } from "@mui/material";
+// import { Box, TextField, Button } from "@mui/material";
+import React from "react";
+import { Chart } from "react-google-charts";
+
+export const data = [
+  ["Task", "Hours per Day"],
+  ["Work", 5],
+  ["Eat", 2],
+  ["Sports", 1.5],
+  ["ITGirls study", 4],
+  ["Sleep", 7.5],
+  ["Read a book", 2],
+  ["Relax", 2],
+];
+
+export const options = {
+  title: "My Daily Activities",
+};
 
 function App() {
   return (
     <div className="App">
-      <Box
-        component="form"
-        sx={{ "& .MuiTextField-root": { m: 1, width: "25ch" } }}
-        noValidate
-        autoComplete="off"
-      >
-        <TextField
-          required
-          id="outlined-required"
-          label="Required"
-          defaultValue="Hello World"
-        />
-        <TextField
-          id="filled-password-input"
-          label="Password"
-          type="password"
-          autoComplete="current-password"
-          variant="filled"
-        />
-      </Box>
-      <Button variant="contained" color="success">
-        Success
-      </Button>
+      <Chart
+        chartType="PieChart"
+        data={data}
+        options={options}
+        width={"100%"}
+        height={"700px"}
+      />
     </div>
   );
 }
